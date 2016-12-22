41914fdd72ca4315f722d31a5f3afa0e6413ee53
1102137126 <1102137126@gm.kuas.edu.tw>
2016-12-22 16:04:59 +0800

    Add files via upload

--- connections_db.php
+++ connections_db.php
@@ -3,10 +3,10 @@
     $db_host = 'db.mis.kuas.edu.tw';    
     $db_name = 's1102137126';    
     $db_user = 's1102137126';    
-    $db_password = '1234';
+    $db = '1234';
     $dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8";
     try {
-        $conn = new PDO($dsn, $db_user, $db_password);
+        $conn = new PDO($dsn, $db_user, $db);
     } catch (Exception $exc) {
         echo $exc->getMessage();
     }
