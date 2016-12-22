a39d18ab151ac48ca4b61d441478c529c0f9ba1e
1102137126 <1102137126@gm.kuas.edu.tw>
2016-12-22 15:57:04 +0800

    sonarqube4 - tab

--- connections_db.php
+++ connections_db.php
@@ -1,13 +1,13 @@
 <?php
-	header("Access-Control-Allow-Origin: *");
-	$db_host = 'db.mis.kuas.edu.tw';    
+    header("Access-Control-Allow-Origin: *");
+    $db_host = 'db.mis.kuas.edu.tw';    
     $db_name = 's1102137126';    
     $db_user = 's1102137126';    
     $db_password = '1234';
     $dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8";
-	try {
+    try {
         $conn = new PDO($dsn, $db_user, $db_password);
-	} catch (Exception $exc) {
+    } catch (Exception $exc) {
         echo $exc->getMessage();
     }
 ?>
