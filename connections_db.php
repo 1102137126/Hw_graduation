2459b09a99c77252bac58a1dfa4275a0dcb9afe4
1102137126 <1102137126@gm.kuas.edu.tw>
2016-12-22 15:24:27 +0800

    sonarqube3

--- connections_db.php
+++ connections_db.php
@@ -10,16 +10,4 @@
 	} catch (Exception $exc) {
         echo $exc->getMessage();
     }
-
-	/*$db_host = 'localhost';
-	$db_name = 's1102137126';
-	$db_user = 's1102137126';
-	$db_password = '1234';
-	$dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8";
-	try {
-		$conn = new PDO($dsn, $db_user, $db_password);
-	} catch (PDOException $e) {
-		print "Error!: " . $e->getMessage() . "<br/>";
-		die();
-	}*/
 ?>
